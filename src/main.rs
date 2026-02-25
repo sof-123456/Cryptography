64feaf1f2469f5febe5a55dd730147de3fbfff78
sof-123456 <140717160+sof-123456@users.noreply.github.com>
2026-02-25 13:35:24 +0400

    new commit

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ fn rc4(key: &[u8]) {
     // Key-scheduling algorithm (KSA)
     let mut j = 0usize;
     for i in 0..256 {
-        j = (j + s[i] as usize + t[i] as usize) % 256;
-        s.swap(i, j); // swap s[i] and s[j]
+        j = (j + s[i] as usize + t[i] as usize) % 256;  // j<->j 
+        s.swap(i, j); // swap s[i] and s[j]   
     }
 for i in 0..256
 {
@@ -44,12 +44,33 @@ for i in 0..256
 
 fn main() {
     let mut key = [0u8; 256];
-    key[0]=0;
-    key[1]= 0;
-    for i in 1..255 {
-       // key[i] = i as u8;
-      key[i+1] = (256   - i) as u8;   //  all 
-    }
+   //key[0]=0;
+   //key[1]= 0;
+   //for i in 1..255 {
+   //   // key[i] = i as u8;
+   //  key[i+1] = (256   - i) as u8;   //  key[2] = 255 ....
+   //}
+
+
+   key[0] = 2 ;
+   key[1] = 254;
+   key[2] = 255 ;
+   key[3] = 0 ;
+
+   for  k in 1..63 
+   {
+      key[4*k] = ( 256 - (4*k -3 )) as u8;
+      key[4*k+1] = ( 256 - (4*k +2)) as u8;
+      key[4*k+2] = ( 256 - (4*k )) as u8;
+      key[4*k+3] = ( 256 - (4*k + 1 )) as u8;
+   }
+   for i in key{
+    print!("{}  ", i );
+   }
+   println!();
+      println!();
+
+
     rc4(&key);
 }
 
